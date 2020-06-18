728c172c1aa66fa5c85dce15e5ffbc07396f91f2
Qest333 <lopit321@gmail.com>
2020-06-18 12:01:00 +0200

    Added new project

---
+++ Zadanie5/Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetect : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public GameObject PauseB;
+    public Text gameScore;
+    public Text endScore;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameOverPanel.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Box")
+        {
+            endScore.text = gameScore.text;
+            gameOverPanel.gameObject.SetActive(true);
+            Destroy(gameObject);
+            Time.timeScale = 0f;
+            PauseB.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Zadanie5/Assets/Scripts/PointCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    public int points = 0;
+    public Text pointText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = points.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Box")
+        {
+            points += 1;
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+}
